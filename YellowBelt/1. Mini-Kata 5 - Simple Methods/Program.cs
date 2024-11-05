e29ea25e0f8978372c55f47f690a323ae70df63e
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-11-05 13:35:38 +0100

    1. Mini-Kata 5 - Simple Methods
    task done

---
+++ YellowBelt/1. Mini-Kata 5 - Simple Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿class ProgramKata5
+{
+    private static int PlayerHealth { get; set; } = 100;
+    private static int EnemyHealth { get; set; } = 100;
+    
+    static void Attack(int damage)
+    {
+        EnemyHealth -= damage;
+        Console.WriteLine($"The player dealt {damage} damage");
+    }
+    
+    static void Heal(int healAmounnt)
+    {
+        PlayerHealth += healAmounnt;
+        Console.WriteLine($"The player healed {healAmounnt} HP");
+    }
+    
+    static void DisplayStats()
+    {
+        Console.WriteLine("--------------------------------------");
+        Console.WriteLine($"Current player health: {PlayerHealth}");
+        Console.WriteLine($"Current enemy health: {EnemyHealth}");
+        Console.WriteLine("--------------------------------------");
+    }
+    
+    static void Main()
+    {
+        DisplayStats();
+        Attack(10);
+        Heal(20);
+        DisplayStats();
+    }
+}
+
+
+
+
+
+
+    
+
